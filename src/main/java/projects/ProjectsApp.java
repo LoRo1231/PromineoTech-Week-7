942c7a5fe01cf02c5f88e52817cd0f37019d823d
Logan Robertson <logan@Logans-MacBook-Pro-2.local>
2024-02-24 20:45:57 -0500

    commiting project

---
+++ src/main/java/projects/ProjectsApp.java
@@ -0,0 +1,14 @@
+package projects;
+
+import projects.dao.DbConnection;
+
+public class ProjectsApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		DbConnection.getConnection();
+
+	}
+
+}
---
+++ src/main/java/projects/exceptions/DbException.java
@@ -0,0 +1,25 @@
+package projects.exceptions;
+
+public class DbException extends RuntimeException {
+
+	public DbException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public DbException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public DbException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public DbException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+
+}
